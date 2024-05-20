b055a26d1b4057d4a1fabfa041ab72969d7033c1
17kumarrupesh <55622346+17kumarrupesh@users.noreply.github.com>
2024-05-20 06:31:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = "terraformtest"
   location = "Central India"
 }
